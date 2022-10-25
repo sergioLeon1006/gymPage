b6957f960fa0ad9be47c5be69678e4b41857826f
sergioLeon <sergiosaldarriagadavila@gmail.com>
2022-10-25 13:44:15 -0500

    actualizacion componentes

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -4,7 +4,7 @@ import { Box, Stack, Typography } from '@mui/material';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
-//import Loader from './Loader';
+import Loader from './Loader';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   
@@ -38,7 +38,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     window.scrollTo({ top: 1800, behavior: 'smooth' });
   };
 
-  //if (!currentExercises.length) return <Loader />;
+  if (!currentExercises.length) return <Loader />;
 
   return (
     <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -12,3 +12,12 @@ export const fetchData = async (url, options) => {
     const data = await response.json();
     return data;
 }
+
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY, 
+    },
+  };
+  
