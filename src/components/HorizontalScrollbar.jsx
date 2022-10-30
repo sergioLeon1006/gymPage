b719e2ecd58857939d6d798eacabc42509ba1dc4
sergioLeon1006 <sergiosaldarriagadavila@gmail.com>
2022-10-30 10:24:40 -0500

    complete demo app

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -6,19 +6,21 @@ import ExerciseCard from './ExerciseCard';
 import BodyPart from './BodyPart';
 
 
-const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
-  <ScrollMenu>
-    {data.map((item) => (
-      <Box
-        key={item.id || item}
-        itemID={item.id || item}
-        title={item.id || item}
-        m="0 40px"
-      >
-        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
-      </Box>
-    ))}
-  </ScrollMenu>
-);
-
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => {
+  
+  return (
+    <ScrollMenu>
+      {data.map((item) => (
+        <Box
+          key={item.id || item}
+          itemID={item.id || item}
+          title={item.id || item}
+          m="0 40px"
+        >
+          {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
+        </Box>
+      ))}
+    </ScrollMenu>
+  );
+}
 export default HorizontalScrollbar;
