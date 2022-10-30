7127ec354f5fddc8402ea5bc1356fcc8a6b8ba86
sergioLeon1006 <sergiosaldarriagadavila@gmail.com>
2022-10-30 11:18:53 -0500

    cabio index en navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import {Stack} from '@mui/material'
 
-import Logo from '../assets/images/logo.png'
+import Logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
   return (
